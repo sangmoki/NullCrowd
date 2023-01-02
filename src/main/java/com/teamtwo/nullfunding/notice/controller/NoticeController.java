3fe21179cbc30f7bf1e1bd7ee09ebf537ce4db49
SeongIlCho <creba1234@gmail.com>
2023-01-02 10:40:38 +0900

    .

--- src/main/java/com/teamtwo/nullfunding/notice/controller/NoticeController.java
+++ src/main/java/com/teamtwo/nullfunding/notice/controller/NoticeController.java
@@ -21,74 +21,23 @@ public class NoticeController {
         this.noticeService = noticeService;
     }
 
+    // 공지사항 리스트 조회하는 용도의 메서드
     @GetMapping("/list")
     public ModelAndView noticeList(ModelAndView mv) {
-        log.info("");
-        log.info("");
-        log.info("[NoticeController] =========================================================");
 
         List<NoticeDTO> noticeList = noticeService.selectAllNoticeList();
-                log.info("[NoticeController] noticeList : " + noticeList);
 
         mv.addObject("noticeList", noticeList);
 
-        mv.setViewName("content/notice/noticeList");
-
-        log.info("[NoticeController] =========================================================");
+        mv.setViewName("notice/list");
 
         return mv;
     }
-//
-//    @GetMapping("/regist")
-//    public String goRegister() {
-//        return "content/notice/noticeRegist";
-//    }
-//
-//    @PostMapping("/regist")
-//    public String registNotice(@ModelAttribute NoticeDTO notice, RedirectAttributes rttr) throws NoticeRegistException {
-//
-//        log.info("");
-//        log.info("");
-//        log.info("[NoticeController] registBoard =========================================================");
-//        log.info("[NoticeController] registBoard Request : " + notice);
-//
-//        noticeService.registNotice(notice);
-//
-//        rttr.addFlashAttribute("message", "공지사항 등록에 성공하셨습니다!");
-//
-//        log.info("[NoticeController] registBoard =========================================================");
-//
-//        return "redirect:/notice/list";
-//    }
-//
-//    @GetMapping("/detail")
-//    public String selectNoticeDetail(HttpServletRequest request, Model model) {
-//
-//        log.info("");
-//        log.info("");
-//        log.info("[NoticeController] selectNoticeDetail =========================================================");
-//
-//        Long no = Long.valueOf(request.getParameter("no"));
-//        log.info("[NoticeController] selectNoticeDetail No : " + no);
-//
-//        NoticeDTO noticeDetail = noticeService.selectNoticeDetail(no);
-//        log.info("[NoticeController] noticeDetail : " + noticeDetail);
-//        model.addAttribute("notice", noticeDetail);
-//
-//        log.info("[NoticeController] selectNoticeDetail =========================================================");
-//
-//        return "content/notice/noticeDetail";
-//    }
-//
-//    @GetMapping("/update")
-//    public String goModifyNotice(HttpServletRequest request, Model model) {
-//
-//        log.info("");
-//        log.info("");
-//        log.info("[NoticeController] modifyNotice =========================================================");
-//
-//        Long no = Long.valueOf(request.getParameter("no"));
-//
-//        NoticeDTO notice = noticeService.selectNoticeDetail(no);
+
+    // 공지사항 추가하는 용도의 메서드
+    @GetMapping("/regist")
+    public String goRegister() {
+        return "content/notice/noticeInsert";
+    }
 
 }
--- src/main/java/com/teamtwo/nullfunding/notice/service/NoticeService.java
+++ src/main/java/com/teamtwo/nullfunding/notice/service/NoticeService.java
@@ -23,6 +23,50 @@ public class NoticeService {
         return noticeList;
     }
 
+    /* 공지사항 등록용 메소드 */
+    @Transactional
+    public void registNotice(NoticeDTO notice) /*throws NoticeRegistException */{
+
+        int result = mapper.insertNotice(notice);
+
+        if(!(result > 0)) {
+            throw new NoticeRegistException("공지사항 등록 실패 !");
+        }
+    }
+
+    /* 공지사항 수정용 메소드 */
+    @Transactional
+    public void updateNotice(NoticeDTO notice) /*throws NoticeModifyException */{
+        int result = mapper.updateNotice(notice);
+
+        if(!(result > 0)) {
+            throw new NoticeModifyException("공지사항 수정 실패 !");
+        }
+    }
+
+    /* 공지사항 삭제용 메소드 */
+    @Transactional
+    public void removeNotice(Long no) throws NoticeRemoveException {
+        int result = mapper.deleteNotice(no);
+
+        if(!(result > 0)) {
+            throw new NoticeRemoveException("공지사항 삭제 실패 !");
+        }
+    }
+
+    /* 공지사항 상세 페이지 조회용 메소드 */
+    public NoticeDTO selectChoiceNotice(Long count) {
+        NoticeDTO choiceNotice = null;
+
+        int result = mapper.incrementNoticeCount(count);
+
+        if (result > 0) {
+            choiceNotice = mapper.selectChoiceNotice(count);
+        }
+
+        return choiceNotice;
+    }
+
 //    /* 공지사항 등록용 메소드 */
 //    @Transactional
 //    public void registNotice(NoticeDTO notice) throws NoticeRegistException {
