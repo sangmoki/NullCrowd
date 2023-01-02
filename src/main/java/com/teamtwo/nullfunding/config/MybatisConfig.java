96313f5a0a3f464a6042d142e5701ba798a3dc56
hyeongoo <hyeongoo1540@gmail.com>
2023-01-02 09:59:45 +0900

    현구

--- src/main/java/com/teamtwo/nullfunding/config/MybatisConfig.java
+++ src/main/java/com/teamtwo/nullfunding/config/MybatisConfig.java
@@ -1,8 +1,8 @@
 package com.teamtwo.nullfunding.config;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
-
 
 @Configuration
 @MapperScan(basePackages = "com.teamtwo.nullfunding", annotationClass = Mapper.class)
