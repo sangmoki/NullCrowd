f02163a8961d7364f4806f7ae07d836ac36bbbfc
SeongIlCho <creba1234@gmail.com>
2023-01-03 12:48:30 +0900

    성일 중간커밋

--- src/main/java/com/teamtwo/nullfunding/member/controller/MemberController.java
+++ src/main/java/com/teamtwo/nullfunding/member/controller/MemberController.java
@@ -3,14 +3,24 @@ package com.teamtwo.nullfunding.member.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/member/*")
+@RequestMapping("/member")
 public class MemberController {
 
     @GetMapping("/signup")
-    public String signup(){
-        return "signup";
+    public String signupMember(){
+
+        return "content/member/signup";
+    }
+
+    @GetMapping("/login")
+    public ModelAndView goLogIn(ModelAndView mv){
+
+        mv.setViewName("content/member/login");
+
+        return mv;
     }
 }
 
