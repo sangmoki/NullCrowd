673dd3695c8941a31f10ebd63fc80dab49a0ee4f
서도원 <helper9050@gmail.com>
2023-01-03 13:16:18 +0900

    점심 전 커밋

--- src/main/java/com/teamtwo/nullfunding/community/model/dto/ComcommentDTO.java
+++ src/main/java/com/teamtwo/nullfunding/community/model/dto/ComcommentDTO.java
@@ -15,6 +15,7 @@ public class ComcommentDTO {
     private int commentNo; // 댓글번호(시퀀스)
     private int articleNo; // 게시글번호(시퀀스)
     private int memCode; // 회원번호(시퀀스)
+    private String nickName; // 회원닉네임
     private String content; // 내용
     private Date uploadDate; // 작성일
     private MemberDTO writer;
--- src/main/java/com/teamtwo/nullfunding/community/model/dto/CommunityDTO.java
+++ src/main/java/com/teamtwo/nullfunding/community/model/dto/CommunityDTO.java
@@ -1,9 +1,6 @@
 package com.teamtwo.nullfunding.community.model.dto;
 
-<<<<<<< HEAD
 import com.teamtwo.nullfunding.member.dto.MemberDTO;
-=======
->>>>>>> origin/master
 import lombok.*;
 
 import java.util.Date;
@@ -17,11 +14,9 @@ public class CommunityDTO {
 
     private int commentNo; // 게시글 번호
     private int memCode; // 회원번호(시퀀스), 닉네임?
+    private String nickName; // 회원닉네임
     private String title; // 제목
     private String content; // 내용
     private Date uploadDate; // 작성일
-<<<<<<< HEAD
     private MemberDTO writer;
-=======
->>>>>>> origin/master
 }
