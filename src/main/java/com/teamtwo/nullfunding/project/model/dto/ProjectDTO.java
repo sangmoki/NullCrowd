6131dd6f48b53b2d806c320062ac81197317ab02
SeongIlCho <creba1234@gmail.com>
2023-01-04 22:17:32 +0900

    1/4 성일 커밋

--- src/main/java/com/teamtwo/nullfunding/project/model/dto/ProjectDTO.java
+++ src/main/java/com/teamtwo/nullfunding/project/model/dto/ProjectDTO.java
@@ -13,12 +13,14 @@ import java.util.List;
 public class ProjectDTO {
 
     private int projectNo;
+    private int raiserCode;
     private String description;  //프로젝트 content 부분
     private int fundGoal; // 목표금액
     private Date regiDate; // 등록 날짜
     private Date startDate; //시작 날짜
     private Date endDate; //종료 날짜
     private char isFundable; //후원가능여부
+    private int funderNum; // 참여자수
 
     private List<ProjectMediaDTO> projectMediaList;
 }
