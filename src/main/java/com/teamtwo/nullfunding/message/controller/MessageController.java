846c4a6b86b61734d58ec113c3b809de5744890c
boseong kim <boseongzzzzz@gamil.com>
2023-01-05 10:42:58 +0900

    boseong first commit

--- src/main/java/com/teamtwo/nullfunding/message/controller/MessageController.java
+++ src/main/java/com/teamtwo/nullfunding/message/controller/MessageController.java
@@ -1,4 +1,15 @@
 package com.teamtwo.nullfunding.message.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/message")
 public class MessageController {
+
+//    private final ;
+
+    public MessageController()
+
 }
+
