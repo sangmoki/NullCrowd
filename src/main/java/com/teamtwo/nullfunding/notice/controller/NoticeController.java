c895bfdad63191fa2e78cf31447be8eb6e78eda3
sangmoki <simbaesim@gmail.com>
2023-01-07 14:00:31 +0900

    상목 다시 복구중, 페이징 처리 완료, 검색 및 상세보기 부분 처리중

--- src/main/java/com/teamtwo/nullfunding/notice/controller/NoticeController.java
+++ src/main/java/com/teamtwo/nullfunding/notice/controller/NoticeController.java
@@ -1,5 +1,7 @@
 package com.teamtwo.nullfunding.notice.controller;
 
+import com.teamtwo.nullfunding.common.paging.Pagenation;
+import com.teamtwo.nullfunding.common.paging.SelectCriteria;
 import com.teamtwo.nullfunding.member.dto.UserImpl;
 import com.teamtwo.nullfunding.notice.model.dto.NoticeDTO;
 import com.teamtwo.nullfunding.notice.service.NoticeService;
@@ -13,10 +15,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/notice")
@@ -32,52 +35,67 @@ public class NoticeController {
 
     // 모든 공지사항 조회
     @GetMapping("/list")
-    public ModelAndView noticeList(ModelAndView mv) {
+    public ModelAndView noticeList(@RequestParam(defaultValue = "") String searchCondition,
+                                   @RequestParam(defaultValue = "") String searchValue,
+                                   @RequestParam(value="currentPage", defaultValue = "1") int pageNo, ModelAndView mv) {
 
-        List<NoticeDTO> noticeList = noticeService.selectAllNoticeList();
+      /* 목록보기를 눌렀을 시 가장 처음에 보여지는 페이지는 1페이지이다.
+       * 파라미터로 전달되는 페이지가 있는 경우 currentPage는 파라미터로 전달받은 페이지 수 이다.
+       */
+        Map<String, String> searchMap = new HashMap<>();
+        searchMap.put("searchCondition", searchCondition);
+        searchMap.put("searchValue", searchValue);
+
+        /* 전체 게시물 수가 필요하다.
+         * 데이터베이스에서 먼저 전체 게시물 수를 조회해올 것이다.
+         * 검색조건이 있는 경우 검색 조건에 맞는 전체 게시물 수를 조회한다.
+         */
+        int totalCount = noticeService.selectTotalCount(searchMap); // Map으로 생성한 searchMap객체를 보내 service에서 담아 갖고 와 totalCount에 담아준다,.
+
+        /* 한 페이지에 보여 줄 게시물 수 */
+        int limit = 10;		//얘도 파라미터로 전달받아도 된다.
+
+        /* 한 번에 보여질 페이징 버튼의 갯수 */
+        int buttonAmount = 5;
+
+        /* 페이징 처리를 위한 로직 호출 후 페이징 처리에 관한 정보를 담고 있는 인스턴스를 반환받는다. */
+        SelectCriteria selectCriteria = null;
+
+        if(searchCondition != null && !"".equals(searchCondition)) { // 검색조건이 null이 아니고 검색조건과 동일하면 검색조건으로 처리한 select 쿼리문을 들고와 담아준다.
+            selectCriteria = Pagenation.getSelectCriteria(pageNo, totalCount, limit, buttonAmount, searchCondition, searchValue);
+        } else {  // 만약 둘중 하나라도 아니면 아래 로직을 담는다.
+            selectCriteria = Pagenation.getSelectCriteria(pageNo, totalCount, limit, buttonAmount);
+        }
+
+        List<NoticeDTO> noticeList = noticeService.selectAllNoticeList(selectCriteria);
         mv.addObject("noticeList", noticeList);
+        mv.addObject("selectCriteria", selectCriteria);
 
         mv.setViewName("content/notice/noticeList");
 
         return mv;
     }
 
-    // 공지사항 검색하여 조회
-//    @GetMapping("/list")
-//    public ModelAndView selectChoiceNotice(ModelAndView mv){
-//
-//        List<NoticeDTO> noticeList = noticeService.selectChoiceNotice();
-//        mv.addObject("noticeList", noticeList);
-//
-//        mv.setViewName("content/notice/noticeList");
-//
-//        return mv;
-//    }
 
     // 공지사항 상세보기 페이지
-//    @GetMapping("/list")
-//    public ModelAndView goChoiceNotice(ModelAndView mv) {
-//
-//        int no = 0;
-//
-//        List<NoticeDTO> noticeList = noticeService.selectChoiceNotice(no);
-//        mv.addObject("noticeList", noticeList);
-//
-//        mv.setViewName("content/notice/noticeList");
-//
-//        return mv;
-//    }
+    @GetMapping("/detail")
+    public String selectNoticeDetail(HttpServletRequest request, Model model) {
+
+        int no = Integer.valueOf(request.getParameter("no"));
+
+        NoticeDTO noticeDetail = noticeService.selectNoticeDetail(no);
+
+        model.addAttribute("detail", noticeDetail);
+
+        System.out.println("noticeDetail = " + noticeDetail);
+
+        return "content/notice/noticeDetail";
+    }
 
 
 //     공지사항 추가하는 용도의 메서드
     @GetMapping("insert")
     public String goInsert( Model model) {
-
-
-//        System.out.println("userDetails = " +  ((UserImpl)userDetails));
-
-
-//        String memCode = noticeService.insertNoticeCode(((UserImpl)userDetails).getMemCode());
 
         return "content/notice/noticeInsert";
     }
@@ -86,12 +104,10 @@ public class NoticeController {
 
     @PostMapping("insert")
     public String insertNotice(@ModelAttribute NoticeDTO notice
-            , RedirectAttributes rttr, @AuthenticationPrincipal UserDetails userDetails) {
+            , @AuthenticationPrincipal UserDetails userDetails) {
         int memberCode = ((UserImpl)userDetails).getMemCode();
-        System.out.println("memberCode = " + memberCode);
         notice.setMemberCode(memberCode);
         noticeService.insertNotice(notice);
-        rttr.addFlashAttribute("message", "공지사항 등록에 성공하셨습니다!");
 
         return "redirect:/notice/list";
     }
