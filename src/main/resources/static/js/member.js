8b26b7b3fbe489a24479f68edecb0f4f3f752cc9
hyeongoo <hyeongoo1540@gmail.com>
2023-01-09 12:05:37 +0900

    마스터 올리기전 커밋

--- src/main/resources/static/js/member.js
+++ src/main/resources/static/js/member.js
@@ -3,25 +3,27 @@ let idCheck = false;
 
 
 // 아이디 중복 체크
-$('#memEmail').on('click', function () {
+$(function() {
+    $('#memEmail').on('click', function () {
 
-    let memEmail = $('#memEmail').val();
+        let memEmail = $('#memEmail').val();
 
-    $.ajax({
-        url: "/member/idDupCheck",
-        type: 'post',
-        data: {memEmail: memEmail},
-        success: function (result) {
-            if (result == 0) {
-                $('.idCheck').html('사용 가능한 아이디입니다.');
-                idCheck = true;
-            } else {
-                $('.idCheck').html('이미 사용하고 있는 이메일입니다.');
-                idCheck = false;
+        $.ajax({
+            url: "/member/idDupCheck",
+            type: 'post',
+            data: {memEmail: memEmail},
+            success: function (result) {
+                if (result == 0) {
+                    $('.idCheck').html('사용 가능한 아이디입니다.');
+                    idCheck = true;
+                } else {
+                    $('.idCheck').html('이미 사용하고 있는 이메일입니다.');
+                    idCheck = false;
+                }
             }
-        }
-    });
-})
+        });
+    })
+});
 
 
 // 닉네임 중복 체크
