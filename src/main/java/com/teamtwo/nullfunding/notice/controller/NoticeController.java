b670ea6a20f5c89617389744a1ebe6ed22fe2abf
sangmoki <simbaesim@gmail.com>
2023-01-09 21:42:41 +0900

    상목 공지사항 완료, 조회수 2씩 증가,, 및 썸머노트 파일부분 제외하고 노티스 완료

--- src/main/java/com/teamtwo/nullfunding/notice/controller/NoticeController.java
+++ src/main/java/com/teamtwo/nullfunding/notice/controller/NoticeController.java
@@ -6,6 +6,7 @@ import com.teamtwo.nullfunding.member.dto.UserImpl;
 import com.teamtwo.nullfunding.notice.model.dto.NoticeDTO;
 import com.teamtwo.nullfunding.notice.service.NoticeService;
 import com.teamtwo.nullfunding.notice.service.NoticeServiceImpl;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +16,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.lang.model.SourceVersion;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/notice")
@@ -149,6 +156,41 @@ public class NoticeController {
         return "redirect:/notice/list";
 
     }
+
+//    @RequestMapping(value="/uploadSummernoteImageFile", produces = "application/json; charset=utf8")
+//    @ResponseBody
+//    public String uploadSummernoteImageFile(@RequestParam("file") MultipartFile multipartFile, HttpServletRequest request )  {
+//        JsonObject jsonObject = new JsonObject();
+//
+//        /*
+//         * String fileRoot = "C:\\summernote_image\\"; // 외부경로로 저장을 희망할때.
+//         */
+//
+//        // 내부경로로 저장
+//        String contextRoot = new HttpServletRequestWrapper(request).getRealPath("/");
+//        String fileRoot = contextRoot+"resources/fileupload/";
+//
+//        String originalFileName = multipartFile.getOriginalFilename();	//오리지날 파일명
+//        String extension = originalFileName.substring(originalFileName.lastIndexOf("."));	//파일 확장자
+//        String savedFileName = UUID.randomUUID() + extension;	//저장될 파일 명
+//
+//        File targetFile = new File(fileRoot + savedFileName);
+//        try {
+//            InputStream fileStream = multipartFile.getInputStream();
+//            FileUtils.copyInputStreamToFile(fileStream, targetFile);	//파일 저장
+//            jsonObject.addProperty("url", "/summernote/resources/fileupload/"+savedFileName); // contextroot + resources + 저장할 내부 폴더명
+//            jsonObject.addProperty("responseCode", "success");
+//
+//        } catch (IOException e) {
+//            FileUtils.deleteQuietly(targetFile);	//저장된 파일 삭제
+//            jsonObject.addProperty("responseCode", "error");
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        String a = jsonObject.toString();
+//        return a;
+//    }
 
 
 }
