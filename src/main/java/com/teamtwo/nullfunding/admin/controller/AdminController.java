29ef85e071f3df281f0e2cd94dc27db77e7e060e
sangmoki <simbaesim@gmail.com>
2023-01-10 21:26:00 +0900

    상목 관리자페이지 회원관리, 프로젝트관리, 핫펀딩, 문의관리 html 연동 및 ui 구성 완료

--- src/main/java/com/teamtwo/nullfunding/admin/controller/AdminController.java
+++ src/main/java/com/teamtwo/nullfunding/admin/controller/AdminController.java
@@ -12,6 +12,55 @@ import org.springframework.web.servlet.ModelAndView;
 public class AdminController {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
+
+    @GetMapping("/preview")
+    public ModelAndView adminPreview(ModelAndView mv){
+
+        mv.setViewName("content/admin/preview");
+
+        return mv;
+    }
+
+    @GetMapping("/member")
+    public ModelAndView adminMember(ModelAndView mv){
+
+        mv.setViewName("content/admin/member");
+
+        return mv;
+    }
+
+    @GetMapping("/project")
+    public ModelAndView adminProject(ModelAndView mv){
+
+        mv.setViewName("content/admin/project");
+
+        return mv;
+    }
+
+    @GetMapping("/money")
+    public ModelAndView adminMoney(ModelAndView mv){
+
+        mv.setViewName("content/admin/money");
+
+        return mv;
+    }
+
+    @GetMapping("/hot")
+    public ModelAndView adminHot(ModelAndView mv){
+
+        mv.setViewName("content/admin/hot");
+
+        return mv;
+    }
+
+    @GetMapping("/message")
+    public ModelAndView adminMessage(ModelAndView mv){
+
+        mv.setViewName("content/admin/message");
+
+        return mv;
+    }
+
     @GetMapping("/inquiry")
     public ModelAndView adminInquiry(ModelAndView mv) {
 
@@ -19,4 +68,10 @@ public class AdminController {
 
         return mv;
     }
+
+
+
+
+
+
 }
