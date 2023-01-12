321af6c0c04c06bc28afb82878532fcfa1fba572
boseongkim <boseongzzzzz@gmail.com>
2023-01-13 03:56:14 +0900

    boseong's 0112 night commit  : some problems here..

--- src/main/java/com/teamtwo/nullfunding/payment/service/PaymentServiceImpl.java
+++ src/main/java/com/teamtwo/nullfunding/payment/service/PaymentServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-public class PaymentServiceImpl {
+public class PaymentServiceImpl implements PaymentService{
 
     private PaymentMapper mapper;
 
