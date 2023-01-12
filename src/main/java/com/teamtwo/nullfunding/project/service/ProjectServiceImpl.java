8b350a1f243cf897769ff683eea225d58e975d35
SeongIlCho <creba1234@gmail.com>
2023-01-13 00:22:44 +0900

    1

--- src/main/java/com/teamtwo/nullfunding/project/service/ProjectServiceImpl.java
+++ src/main/java/com/teamtwo/nullfunding/project/service/ProjectServiceImpl.java
@@ -44,7 +44,20 @@ public class ProjectServiceImpl implements ProjectService {
     @Override
     public PJDetail selectThisProject(int no) {
 
-        return projectMapper.selectThisProject(no);
+        PJDetail pjDetail = projectMapper.selectThisProject(no);
+
+        /* D-Day 넣어주기 */
+        Date endDate = pjDetail.getProjectDTO().getEndDate();
+        int Dday = CalculateDday(endDate);
+        pjDetail.setRemainDate(Dday);
+        /* 달성률 만들어서 넣어주기 */
+        int fundGoal = pjDetail.getProjectDTO().getFundGoal();
+        int raisedFund = pjDetail.getRaisedFund();
+        int achievePercent = (int)(((double)raisedFund/(double)fundGoal)*100);
+
+        pjDetail.setAchievePercent(achievePercent);
+
+        return pjDetail;
     }
 
     @Override
