1725b78ec12220ae5738c02e47d6ef926828c3f6
hyeongoo <hyeongoo1540@gmail.com>
2023-01-13 11:00:27 +0900

    떙기기전 커밋

--- src/main/java/com/teamtwo/nullfunding/config/SecurityConfig.java
+++ src/main/java/com/teamtwo/nullfunding/config/SecurityConfig.java
@@ -45,8 +45,6 @@ public class SecurityConfig {
          .and()
                 .formLogin()
                 .loginPage("/member/login")
-//                .loginProcessingUrl("/member/login")
-//                .defaultSuccessUrl("/")
                 .successForwardUrl("/")
          .and()
                 .logout()
@@ -56,37 +54,6 @@ public class SecurityConfig {
           .and()
                 .build();
     }
-
-
-    /* 비밀번호 암호화에 사용할 객체 : BCryptPasswordEncoder bean 등록 */
-
-
-
-
-    /* HTTP 요청에 대한 권한 설정 */
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//
-//        /* csrf : 토큰 위주의 공격을 막기 위한 작업 (default가 'on'상태)*/
-//        return http.csrf().disable()
-//                .authorizeHttpRequests()
-//                .anyRequest().permitAll()    // 등록되지 않은 경우로는 누구나 접근 가능
-//                .and()
-//                .formLogin()   // 로그인 form을 따로 이용해 로그인 처리할 것이다.
-//                .loginPage("content/member/login")   // login page로 해당 로그인페이지에서 submit요청하는 경로로 지정하겠다는 의미
-//                .successForwardUrl("/")       // 성공 시 페이지 설정
-//                .and()
-//                .logout()                     // 로그아웃 설정
-//                .logoutRequestMatcher(new AntPathRequestMatcher("content/member/logout"))  // 로그아웃 시 요청 경로
-//                .deleteCookies("JSESSIONID")   // 쿠키 제거
-//                .invalidateHttpSession(true)                        // session정보 무효화
-//                .and()
-//                .exceptionHandling()
-//                // 인가/인증 exception 핸들리설정
-//                .accessDeniedPage("/common/denied")  // 인가되지 않았을 때 - 권한이 없는 기능을 요청했을 때
-//                // 호출될 페이지
-//                .and().build();
-//    }
 
     /* 사용자 인증을 위해서 사용할 service bean 등록 , 사용할 비밀번호 인코딩 방식 설정 */
     @Bean
