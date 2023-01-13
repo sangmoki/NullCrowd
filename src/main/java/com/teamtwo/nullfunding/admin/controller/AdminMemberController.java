c7dd0c15d6a5f3e4182974ea8a458180a1de10ed
sangmoki <simbaesim@gmail.com>
2023-01-13 11:14:40 +0900

    상목 최종 복구 완

--- src/main/java/com/teamtwo/nullfunding/admin/controller/AdminMemberController.java
+++ src/main/java/com/teamtwo/nullfunding/admin/controller/AdminMemberController.java
@@ -122,15 +122,6 @@ public class AdminMemberController {
     }
 
 
-    /* 프로젝트 관리 부분 */
-    @GetMapping("/project")
-    public ModelAndView adminProject(ModelAndView mv){
-
-        mv.setViewName("content/admin/project");
-
-        return mv;
-    }
-
     /* 매출 관리 부분 */
     @GetMapping("/sales")
     public ModelAndView adminSales(ModelAndView mv){
